d4a35a2f91f68952de098cd8843bf539ac186bde
Azmat <azmat239@gmail.com>
2024-04-02 14:42:41 +0530

    create cards for food

--- src/components/ListCards.js
+++ src/components/ListCards.js
@@ -1,7 +1,36 @@
 import React from "react";
 
-const ListCards = () => {
-  return <div>ListCards</div>;
+const ListCards = ({ card }) => {
+  return (
+    <div className="flex gap-2 border border-black rounded-l-xl rounded-r-md ">
+      <div>
+        <img
+          src={card.imgSrc}
+          alt="400"
+          width={100}
+          height={100}
+          className="h-[28vh] w-[20vw] aspect-square rounded-s-xl"
+        />
+      </div>
+      <div className="flex flex-col gap-4 justify-center items-start">
+        <div>
+          <h1 className="font-bold text-red-900 text-lg">{card.name}</h1>
+          <p className="text-slate-700 text-md">{card.des}</p>
+        </div>
+        <div>
+          <select>
+            {card.options &&
+              card.options.length > 0 &&
+              card.options.map((option) => (
+                <option key={option.size} value={option.size}>
+                  {option.size}
+                </option>
+              ))}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListCards;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,79 @@
 import React from "react";
 import Carousal from "../components/Carousal";
+import ListCards from "../components/ListCards";
 
 const Home = () => {
+  const cardList = [
+    {
+      cardsinput: {
+        name: "Pizza",
+        des: "This is the description of the food that is above written ",
+        imgSrc: "./logo.jpg",
+        options: [
+          {
+            size: "small",
+          },
+          {
+            size: "medium",
+          },
+          {
+            size: "large",
+          },
+          {
+            size: "extraLarge",
+          },
+        ],
+      },
+    },
+    {
+      cardsinput: {
+        name: "Burger",
+        des: "This is the description of the food that is above written ",
+        imgSrc: "./logo.jpg",
+        options: [
+          {
+            size: "small",
+          },
+          {
+            size: "medium",
+          },
+          {
+            size: "large",
+          },
+        ],
+      },
+    },
+    {
+      cardsinput: {
+        name: "Burger",
+        des: "This is the description of the food that is above written ",
+        imgSrc: "./logo.jpg",
+        options: [
+          {
+            size: "small",
+          },
+          {
+            size: "medium",
+          },
+          {
+            size: "large",
+          },
+        ],
+      },
+    },
+  ];
   return (
     <div>
       <Carousal />
+      <div className="mt-10 mb-2 ">
+        <h1 className="text-center text-amber-700 font-extrabold text-5xl">
+          FoodList
+        </h1>
+      </div>
+      <div className="grid grid-cols-3 justify-evenly gap-4 mx-4 my-4">
+        {cardList &&
+          cardList.map((list) => <ListCards card={list.cardsinput} />)}
+      </div>
     </div>
   );
 };
